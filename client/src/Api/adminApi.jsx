aa937588d4205c5b49530dea316911f86a537379
SAJID HUSSAIN <53827080+sajidhussaint@users.noreply.github.com>
2023-10-24 16:06:49 +0530

    Update adminApi.jsx

--- client/src/Api/adminApi.jsx
+++ client/src/Api/adminApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const adminApi = axios.create({
-  baseURL: `http://localhost:3000/admin`,
+  baseURL: `https://user-management-mern-gamma.vercel.app/admin`,
 });
 
 // ******** ADMIN SIGN IN DATA PASSING AREA **********//
